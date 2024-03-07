6a9a4af96d8c56382966f7f5ee95b397b85e288b
nisa <sdknsckgz@gmail.com>
2024-03-07 16:46:14 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,21 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.InMemoryDal;
+using System;
+using System.Text.Json.Serialization;
+
+//Entities, DataAccess, Business ve Console katmanlarını oluşturunuz. +
+
+//Bir araba nesnesi oluşturunuz. "Car"
+
+//Özellik olarak : Id, BrandId, ColorId, ModelYear, DailyPrice, Description alanlarını ekleyiniz. (Brand = Marka) +
+
+//InMemory formatta GetById, GetAll, Add, Update, Delete oprasyonlarını yazınız.
+
+//Consolda test ediniz.
+
+CarManager carManager=new CarManager(new InMemoryCarDal());
+
+foreach (var car in carManager.GetAll())
+{
+    Console.WriteLine(car.Description); 
+}
